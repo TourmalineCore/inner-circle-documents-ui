b9ccbb953cdd1b002260d2993428a15cdc9ac98e
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-08 10:03:05 +0500

    test: #8693rdp24: add tests to check the operation of the DatePicker on the personalPage

--- src/features/documents/components/PersonalDocuments/PersonalDocumentsContent.cy.tsx
+++ src/features/documents/components/PersonalDocuments/PersonalDocumentsContent.cy.tsx
@@ -16,6 +16,48 @@ describe('PersonalDocumentContent', () => {
     cy.getByData('date-picker')
       .should('exist');
   });
+
+  it(`
+  GIVEN personal documents page
+  WHEN select next year in date picker component
+  THEN render correct date
+  `, () => {
+    mountComponent();
+    const currentYear = new Date().getFullYear();
+
+    cy.getByData('date-picker-select')
+      .click();
+
+    cy.get('[aria-label="Next Year"]')
+      .click();
+
+    cy.contains('Jan')
+      .click();
+
+    cy.getByData('date-picker-result')
+      .should('have.text', `January ${currentYear + 1}`);
+  });
+
+  it(`
+  GIVEN all compensations page 
+  WHEN select pervious year in date picker component
+  THEN render correct date
+  `, () => {
+    mountComponent();
+    const currentYear = new Date().getFullYear();
+
+    cy.getByData('date-picker-select')
+      .click();
+
+    cy.get('[aria-label="Previous Year"]')
+      .click();
+
+    cy.contains('Sep')
+      .click();
+
+    cy.getByData('date-picker-result')
+      .should('have.text', `September ${currentYear - 1}`);
+  });
 });
 
 function mountComponent() {
