c58062acdb8345e8cb12115af55f33360947ab61
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-09 16:05:36 +0500

    test(DocumentsList): #8693r2zy9: add test

--- src/features/documents/components/DocumentsList/DocumentsList.cy.tsx
+++ src/features/documents/components/DocumentsList/DocumentsList.cy.tsx
@@ -1,10 +1,10 @@
-import { AllDocumentsState } from '../../state/AllDocumentsState';
-import { AllDocumentsStateContext } from '../../state/AllDocumentsStateContext';
+import { AllDocumentsState } from '../AllDocumentsState/AllDocumentsState';
+import { AllDocumentsStateContext } from '../AllDocumentsState/AllDocumentsStateContext';
 import { DocumentsList } from './DocumentsList';
 
 describe('DocumentsList', () => {
   it(`
-    GIVEN all documents page 
+    GIVEN documents page 
     WHEN visit documents page
     THEN render documentList
     `, () => {
@@ -15,7 +15,7 @@ describe('DocumentsList', () => {
   });
 
   it(`
-  GIVEN all documents page 
+  GIVEN documents page 
   WHEN visit documents page
   THEN render documentList list
   `, () => {
@@ -26,7 +26,7 @@ describe('DocumentsList', () => {
   });
 
   it(`
-    GIVEN all documents page 
+    GIVEN documents page 
     WHEN visit documents page
     THEN render documentList element
     `, () => {
@@ -37,7 +37,7 @@ describe('DocumentsList', () => {
   });
 
   it(`
-    GIVEN all documents page 
+    GIVEN documents page 
     WHEN visit documents page
     THEN render documentList item text
     `, () => {
@@ -48,7 +48,7 @@ describe('DocumentsList', () => {
   });
 
   it(`
-    GIVEN all documents page 
+    GIVEN documents page 
     WHEN visit documents page
     THEN documentList item have text
     `, () => {
@@ -56,11 +56,11 @@ describe('DocumentsList', () => {
 
     cy.getByData('documents-list-text')
       .first()
-      .should('have.text', 'file-1');
+      .should('have.text', 'Ivanov I.I');
   });
 
   it(`
-    GIVEN all documents page 
+    GIVEN documents page 
     WHEN visit documents page
     THEN render documentList item download
     `, () => {
@@ -69,6 +69,22 @@ describe('DocumentsList', () => {
     cy.getByData('documents-list-download')
       .should('exist');
   });
+
+  it(`
+    GIVEN all documents page 
+    WHEN click on the download button for the first documenList item
+    THEN correct download file
+    `, () => {
+    mountComponent();
+
+    cy.getByData('documents-list-download')
+      .invoke('attr', 'href')
+      .then((href) => {
+        cy.downloadFile(String(href), 'cypress/download', 'test.pdf');
+      });
+
+    cy.task('deleteFolder', 'cypress/download');
+  });
 });
 
 function mountComponent() {
@@ -76,7 +92,15 @@ function mountComponent() {
   const allDocumentsState = new AllDocumentsState();
 
   allDocumentsState.initialize({
-    documents: ['file-1', 'file-2'],
+    documents: [
+      {
+        id: '1',
+        name: 'Ivanov I.I',
+        date: new Date(),
+        previewLink: '',
+        downloadLink: 'https://drive.usercontent.google.com/u/0/uc?id=1WJ1otCKCJeyLzGiPC-8L65NtWQH9TO0D&export=download',
+      },
+    ],
   });
 
   cy.mount(
