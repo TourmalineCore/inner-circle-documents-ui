43c9fc63069895788eadc265921b995041dcfe8f
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-13 14:46:54 +0500

    test(UploadedDocumentsContent): #8693tn3cd: add test

---
+++ src/features/documents/components/ManagerDocuments/components/UploadedDocuments/UploadedDocumentsContent.cy.tsx
@@ -0,0 +1,27 @@
+import { AllDocumentsState } from '../../../AllDocumentsState/AllDocumentsState';
+import { AllDocumentsStateContext } from '../../../AllDocumentsState/AllDocumentsStateContext';
+import { UploadedDocumentsContent } from './UploadedDocumentsContent';
+
+describe('UploadedDocumentsContent', () => {
+  it(`
+  GIVEN uploaded documents page
+  WHEN visit personal documents page
+  THEN render section uploaded documents content
+  `, () => {
+    mountComponent();
+
+    cy.getByData('uploaded-documents-content')
+      .should('exist');
+  });
+});
+
+function mountComponent() {
+  // eslint-disable-next-line react/jsx-no-constructed-context-values
+  const documentsState = new AllDocumentsState();
+
+  cy.mount(
+    <AllDocumentsStateContext.Provider value={documentsState}>
+      <UploadedDocumentsContent />
+    </AllDocumentsStateContext.Provider>,
+  );
+}
