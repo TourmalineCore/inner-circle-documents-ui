42abe994751f1ddc2716f48dbb3101cdb8fd98ec
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-13 15:13:43 +0500

    feat(UploadedDocumentsContent): #8693tn3cd: add uploader

--- src/features/documents/components/ManagerDocuments/components/UploadedDocuments/UploadedDocumentsContent.tsx
+++ src/features/documents/components/ManagerDocuments/components/UploadedDocuments/UploadedDocumentsContent.tsx
@@ -1,5 +1,13 @@
 export function UploadedDocumentsContent() {
   return (
-    <section className="uploaded-documents-content" data-cy="uploaded-documents-content" />
+    <section className="uploaded-documents-content" data-cy="uploaded-documents-content">
+      <input
+        className="uploaded-documents-content__uploader"
+        data-cy="uploaded-documents-content-uploader"
+        type="file"
+        multiple
+        accept=".pdf"
+      />
+    </section>
   );
 }
