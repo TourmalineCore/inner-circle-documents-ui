7c5234c8caa33c906ce37b1e9fc310c6893caedb
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-13 17:01:40 +0500

    feat(UploadingDocumentsContent): #8693tn3cd: add upload documents and render list documents

--- src/features/documents/components/ManagerDocuments/components/UploadingDocuments/UploadingDocumentsContent.tsx
+++ src/features/documents/components/ManagerDocuments/components/UploadingDocuments/UploadingDocumentsContent.tsx
@@ -3,26 +3,42 @@ import { useContext } from 'react';
 import { AllDocumentsStateContext } from '../../../AllDocumentsState/AllDocumentsStateContext';
 
 export const UploadingDocumentsContent = observer(() => {
-  const { allUploadedDocuments } = useContext(AllDocumentsStateContext);
+  const documentsState = useContext(AllDocumentsStateContext);
 
-  const uploadedDocumentsIsEmpty = allUploadedDocuments.length === 0;
+  const uploadedDocumentsIsEmpty = documentsState.allUploadedDocuments.length === 0;
 
   return (
     <section className="uploading-documents-content" data-cy="uploading-documents-content">
       <input
         className="uploading-documents-content__uploader"
         data-cy="uploading-documents-content-uploader"
+        onChange={handleChange}
         type="file"
         multiple
         accept=".pdf"
       />
-      { !uploadedDocumentsIsEmpty
+      {!uploadedDocumentsIsEmpty
        && (
          <ul
            className="uploading-documents-content__list"
            data-cy="uploading-documents-content-list"
-         />
+         >
+           {documentsState.allUploadedDocuments.map((file) => (
+             <li
+               key={file.name}
+               className="uploading-documents-content__item"
+               data-cy="uploading-documents-content-item"
+             >
+               {file.name}
+             </li>
+           ))}
+         </ul>
        )}
     </section>
   );
+
+  function handleChange(event: any) {
+    const fileList: File[] = Object.values(event.target.files);
+    documentsState.setUploadedDocuments(fileList);
+  }
 });
