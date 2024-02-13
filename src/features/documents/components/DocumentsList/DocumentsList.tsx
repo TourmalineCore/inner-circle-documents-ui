d6aaed1f6a2816520f307bd9f33ed131ff94f09a
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-13 13:50:35 +0500

    refactor(DocumentsList): #8693r2zy9: add multiline

--- src/features/documents/components/DocumentsList/DocumentsList.tsx
+++ src/features/documents/components/DocumentsList/DocumentsList.tsx
@@ -7,18 +7,35 @@ export function DocumentsList({
   list: DocumentsProps
 }) {
   return (
-    <div className="documents-list" data-cy="documents-list">
-      <ul className="documents-list__list" data-cy="documents-list-list">
-        {list.map(({ id, name, downloadLink }) => (
+    <div
+      className="documents-list"
+      data-cy="documents-list"
+    >
+      <ul
+        className="documents-list__list"
+        data-cy="documents-list-list"
+      >
+        {list.map(({
+          id,
+          name,
+          downloadLink,
+        }) => (
           <li
             key={id}
             className="documents-list__item"
             data-cy="documents-list-item"
           >
-            <span className="documents-list__text" data-cy="documents-list-text">
+            <span
+              className="documents-list__text"
+              data-cy="documents-list-text"
+            >
               {name}
             </span>
-            <a href={downloadLink} className="documents-list__download" data-cy="documents-list-download">
+            <a
+              href={downloadLink}
+              className="documents-list__download"
+              data-cy="documents-list-download"
+            >
               <IconFileDownload />
             </a>
           </li>
