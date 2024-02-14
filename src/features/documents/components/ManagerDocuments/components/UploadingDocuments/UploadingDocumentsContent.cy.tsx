c83259dd1083492a40368e6ba78de6eaa85f569a
Yasnovsky <andrey5.1997@mail.ru>
2024-02-14 23:32:32 +0500

    test: #8693tn3cd: trying to write a test for downloading files

--- src/features/documents/components/ManagerDocuments/components/UploadingDocuments/UploadingDocumentsContent.cy.tsx
+++ src/features/documents/components/ManagerDocuments/components/UploadingDocuments/UploadingDocumentsContent.cy.tsx
@@ -28,32 +28,38 @@ describe('UploadingDocumentsContent', () => {
       .should('not.exist');
   });
 
-  // it(`
-  // GIVEN uploading documents page
-  // WHEN upload file
-  // THEN correct upload file
-  // `, () => {
-  //   mountComponent();
+  it(`
+  GIVEN uploading documents page
+  WHEN upload file
+  THEN correct upload file
+  `, () => {
+    mountComponent();
 
-  //   cy.get('input[type=file]').selectFile([{
-  //     contents: Cypress.Buffer.from('Иванов'),
-  //     fileName: 'Расчетный листок Иванов за ноябрь 2023.pdf',
-  //     lastModified: Date.now(),
-  //   }, {
-  //     contents: Cypress.Buffer.from('Сидоров'),
-  //     fileName: 'Расчетный листок Сидоров за ноябрь 2023.pdf',
-  //     lastModified: Date.now(),
-  //   }]);
+    // https://docs.cypress.io/guides/core-concepts/writing-and-organizing-tests#Fixture-Files
+    cy.get('input[type=file]').selectFile([
+      'cypress/fixtures/document.pdf',
+      'cypress/fixtures/document.pdf',
+    ]);
 
-  //   cy.getByData('uploading-documents-content-list')
-  //     .should('exist');
+    //   cy.get('input[type=file]').selectFile([{
+    //     contents: Cypress.Buffer.from('Иванов'),
+    //     fileName: 'Расчетный листок Иванов за ноябрь 2023.pdf',
+    //     lastModified: Date.now(),
+    //   }, {
+    //     contents: Cypress.Buffer.from('Сидоров'),
+    //     fileName: 'Расчетный листок Сидоров за ноябрь 2023.pdf',
+    //     lastModified: Date.now(),
+    //   }]);
 
-  //   cy.getByData('uploading-documents-content-item')
-  //     .should('have.length', 2);
+    cy.getByData('uploading-documents-content-list')
+      .should('exist');
 
-  //   cy.getByData('uploaded-document-card')
-  //     .should('have.length', 2);
-  // });
+    cy.getByData('uploading-documents-content-item')
+      .should('have.length', 2);
+
+    cy.getByData('uploaded-document-card')
+      .should('have.length', 2);
+  });
 
   // it(`
   // GIVEN uploading documents page
