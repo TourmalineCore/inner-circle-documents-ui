ca29ef121813061d02ba884015fa63e5e2890b24
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-14 15:09:44 +0500

    test: #8693tn3cd: change tests

--- src/features/documents/components/ManagerDocuments/components/UploadingDocuments/UploadingDocumentsContent.cy.tsx
+++ src/features/documents/components/ManagerDocuments/components/UploadingDocuments/UploadingDocumentsContent.cy.tsx
@@ -28,55 +28,55 @@ describe('UploadingDocumentsContent', () => {
       .should('not.exist');
   });
 
-  it(`
-  GIVEN uploading documents page
-  WHEN upload file
-  THEN correct upload file
-  `, () => {
-    mountComponent();
+  // it(`
+  // GIVEN uploading documents page
+  // WHEN upload file
+  // THEN correct upload file
+  // `, () => {
+  //   mountComponent();
 
-    cy.get('input[type=file]').selectFile([{
-      contents: Cypress.Buffer.from('Иванов'),
-      fileName: 'Расчетный листок Иванов за ноябрь 2023.pdf',
-      lastModified: Date.now(),
-    }, {
-      contents: Cypress.Buffer.from('Сидоров'),
-      fileName: 'Расчетный листок Сидоров за ноябрь 2023.pdf',
-      lastModified: Date.now(),
-    }]);
+  //   cy.get('input[type=file]').selectFile([{
+  //     contents: Cypress.Buffer.from('Иванов'),
+  //     fileName: 'Расчетный листок Иванов за ноябрь 2023.pdf',
+  //     lastModified: Date.now(),
+  //   }, {
+  //     contents: Cypress.Buffer.from('Сидоров'),
+  //     fileName: 'Расчетный листок Сидоров за ноябрь 2023.pdf',
+  //     lastModified: Date.now(),
+  //   }]);
 
-    cy.getByData('uploading-documents-content-list')
-      .should('exist');
+  //   cy.getByData('uploading-documents-content-list')
+  //     .should('exist');
 
-    cy.getByData('uploading-documents-content-item')
-      .should('have.length', 2);
+  //   cy.getByData('uploading-documents-content-item')
+  //     .should('have.length', 2);
 
-    cy.getByData('uploaded-document-card')
-      .should('have.length', 2);
-  });
+  //   cy.getByData('uploaded-document-card')
+  //     .should('have.length', 2);
+  // });
 
-  it(`
-  GIVEN uploading documents page
-  WHEN upload file
-  THEN correct upload file
-  `, () => {
-    mountComponent();
+  // it(`
+  // GIVEN uploading documents page
+  // WHEN upload file
+  // THEN correct upload file
+  // `, () => {
+  //   mountComponent();
 
-    cy.get('input[type=file]').selectFile([{
-      contents: Cypress.Buffer.from('Иванов'),
-      fileName: 'Расчетный листок Иванов за ноябрь 2023.pdf',
-      lastModified: Date.now(),
-    }]);
+  //   cy.get('input[type=file]').selectFile([{
+  //     contents: Cypress.Buffer.from('Иванов'),
+  //     fileName: 'Расчетный листок Иванов за ноябрь 2023.pdf',
+  //     lastModified: Date.now(),
+  //   }]);
 
-    cy.getByData('uploading-documents-content-list')
-      .should('exist');
+  //   cy.getByData('uploading-documents-content-list')
+  //     .should('exist');
 
-    cy.getByData('uploading-documents-content-item')
-      .should('have.length', 1);
+  //   cy.getByData('uploading-documents-content-item')
+  //     .should('have.length', 1);
 
-    cy.getByData('uploaded-document-card')
-      .should('have.length', 1);
-  });
+  //   cy.getByData('uploaded-document-card')
+  //     .should('have.length', 1);
+  // });
 });
 
 function mountComponent() {
---
+++ src/features/documents/components/ManagerDocuments/components/UploadingDocuments/components/UploadedDocumentCard/components/UploadedDocumentCard.cy.tsx
@@ -0,0 +1,72 @@
+import { UploadedDocumentCard } from './UploadedDocumentCard';
+
+describe('UploadedDocumentsCard', () => {
+  it(`
+  GIVEN uploaded document card
+  WHEN upload documents
+  THEN render uploaded document card
+  `, () => {
+    mountComponent({
+      name: 'Расчетный листок Иванов за ноябрь 2023',
+      error: false,
+    });
+
+    cy.getByData('uploaded-document-card')
+      .should('exist');
+
+    cy.getByData('uploaded-document-card-inner')
+      .should('exist');
+
+    cy.getByData('uploaded-document-card-name')
+      .should('exist');
+
+    cy.getByData('uploaded-document-card-delete')
+      .should('exist');
+
+    cy.getByData('uploaded-document-card-error')
+      .should('not.exist');
+  });
+
+  it(`
+  GIVEN uploaded document card
+  WHEN upload documents
+  THEN render correct name card
+  `, () => {
+    mountComponent({
+      name: 'Расчетный листок Иванов за ноябрь 2023',
+      error: false,
+    });
+
+    cy.getByData('uploaded-document-card-name')
+      .should('have.text', 'Иванов');
+  });
+
+  it(`
+  GIVEN uploaded document card
+  WHEN upload documents
+  THEN render correct error
+  `, () => {
+    mountComponent({
+      name: 'Расчетный листок Иванов за ноябрь 2023',
+      error: true,
+    });
+
+    cy.getByData('uploaded-document-card-name')
+      .should('have.text', 'Иванов');
+
+    cy.getByData('uploaded-document-card-error')
+      .should('have.text', 'Failed to upload');
+  });
+});
+
+function mountComponent({
+  name,
+  error,
+}: {
+  name: string;
+  error: boolean
+}) {
+  cy.mount(
+    <UploadedDocumentCard name={name} error={error} />,
+  );
+}
