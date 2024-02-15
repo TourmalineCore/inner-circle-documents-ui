70ae114a5e60dd47479d782f52b14f805c606b87
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-15 14:48:48 +0500

    chore: #8693tn3cd: add icon and style for UploadedDocumentCard

--- src/features/documents/components/ManagerDocuments/components/UploadingDocuments/components/UploadedDocuments/components/UploadedDocumentCard/UploadedDocumentCard.tsx
+++ src/features/documents/components/ManagerDocuments/components/UploadingDocuments/components/UploadedDocuments/components/UploadedDocumentCard/UploadedDocumentCard.tsx
@@ -1,5 +1,6 @@
 import { pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
+import IconDelet from '../../../../../../../../../../assets/icons/icon-delete.svg';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -21,16 +22,27 @@ export function UploadedDocumentCard({
         className="uploaded-document-card__inner"
         data-cy="uploaded-document-card-inner"
       >
-        <h3
-          className="uploaded-document-card__name"
-          data-cy="uploaded-document-card-name"
-        >
-          {name.split(' ')[2]}
-        </h3>
-        <span
-          className="uploaded-document-card__delete"
-          data-cy="uploaded-document-card-delete"
-        />
+        <div className="uploaded-document-card__header">
+          <h3
+            className="uploaded-document-card__name"
+            data-cy="uploaded-document-card-name"
+          >
+            {name.split(' ')[2]}
+          </h3>
+          <span
+            className="uploaded-document-card__delete"
+            data-cy="uploaded-document-card-delete"
+          >
+            <img
+              className="uploaded-document-card__icon"
+              data-cy="uploaded-document-card-icon"
+              src={IconDelet}
+              width="20"
+              height="20"
+              alt="DeleteIcon"
+            />
+          </span>
+        </div>
         {error && (
           <span
             className="uploaded-document-card__error"
