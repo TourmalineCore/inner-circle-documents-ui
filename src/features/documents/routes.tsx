791d8d0e1f057aa80bed5d3571974027e2a69051
Dmitriy Myakotin <demon4301@gmail.com>
2024-02-26 10:01:21 +0500

    refactor: #8693: change breadcrumbs

--- src/features/documents/routes.tsx
+++ src/features/documents/routes.tsx
@@ -1,18 +1,13 @@
 import { ReactComponent as IconProfile } from '../../assets/icons/icon-profile.svg';
 import { ReactComponent as IconProfileActive } from '../../assets/icons/icon-profile-active.svg';
 import { DocumentsPage } from './DocumentsPage';
-import { UploadingDocumentsPage } from './UploadingDocumentsPage';
 
+// TODO figure out how to make custom breadcrumbs for nested routers
 export const documentsRoutes = [
   {
     path: '/',
     breadcrumb: 'MailingPayslips',
     Component: DocumentsPage,
-  },
-  {
-    path: '/uploading-documents',
-    breadcrumb: 'UploadingDocuments',
-    Component: UploadingDocumentsPage,
   },
 ];
 
