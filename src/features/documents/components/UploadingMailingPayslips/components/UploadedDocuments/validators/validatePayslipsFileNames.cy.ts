6c58ff23ecda1b2f0b742a5818474ddf6cda378f
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-06-10 14:32:35 +0500

    tests: add test where single file and single employee last name matched

---
+++ src/features/documents/components/UploadingMailingPayslips/components/UploadedDocuments/validators/validatePayslipsFileNames.cy.ts
@@ -0,0 +1,19 @@
+describe('validatePayslipsFileNames', () => {
+  it(`
+  GIVEN single file 
+  AND single employee
+  WHEN last name matched 
+  THEN no validation errors
+  `, () => {
+    expect(validatePayslipsFileNames({
+      payslipFileNames: [
+        'Расчетный листок за ноябрь Иванов 2023.pdf',
+      ],
+      employees: [
+        {
+          lastName: 'Иванов',
+        },
+      ],
+    }).length).eq(0);
+  });
+});
