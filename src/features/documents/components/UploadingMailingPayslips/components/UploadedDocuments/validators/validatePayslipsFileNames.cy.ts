12adb12cd6429064bc9748d60b6ac9777c67dc4e
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-06-10 16:09:47 +0500

    test: validatePayslipsFileNames.cy.ts
    GIVEN two employees
    AND two files with employee's last name in lowercase and uppercase in filename
    WHEN last name matched
    THEN no validation errors

--- src/features/documents/components/UploadingMailingPayslips/components/UploadedDocuments/validators/validatePayslipsFileNames.cy.ts
+++ src/features/documents/components/UploadingMailingPayslips/components/UploadedDocuments/validators/validatePayslipsFileNames.cy.ts
@@ -22,7 +22,7 @@ describe('validatePayslipsFileNames', () => {
   it(`
   GIVEN single file 
   AND no employee with this last name
-  WHEN last name matched 
+  WHEN last name not matched
   THEN returns one validation error for index 0
   `, () => {
     expect(validatePayslipsFileNames({
@@ -35,5 +35,27 @@ describe('validatePayslipsFileNames', () => {
         },
       ],
     })).to.deep.equal([0]);
+  });
+
+  it(`
+  GIVEN two employees
+  AND two files with employee's last name in lowercase and uppercase in filename
+  WHEN last name matched
+  THEN no validation errors
+  `, () => {
+    expect(validatePayslipsFileNames({
+      payslipFileNames: [
+        'Расчетный листок за ноябрь ПЕТРОВ 2023.pdf',
+        'Расчетный листок за ноябрь иванов 2023.pdf',
+      ],
+      employees: [
+        {
+          lastName: 'Иванов',
+        },
+        {
+          lastName: 'Петров',
+        },
+      ],
+    }).length).eq(0);
   });
 });
