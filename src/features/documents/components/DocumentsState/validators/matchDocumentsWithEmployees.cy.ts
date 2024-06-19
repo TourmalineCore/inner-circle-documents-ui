71e88a6d45c926cd22b16a4a3104dd137d34d659
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-06-19 09:45:48 +0500

    test: matchDocumentsWithEmployees.cy.ts
      GIVEN single file
      AND single employee
      WHEN last name matched
      THEN map contains one id-employee relation

--- src/features/documents/components/DocumentsState/validators/matchDocumentsWithEmployees.cy.ts
+++ src/features/documents/components/DocumentsState/validators/matchDocumentsWithEmployees.cy.ts
@@ -18,4 +18,31 @@ describe('matchDocumentsWithEmployees', () => {
       employees: [],
     })).to.deep.equal({});
   });
+
+  it(`
+  GIVEN single file 
+  AND single employee
+  WHEN last name matched 
+  THEN map contains one id-employee relation
+  `, () => {
+    expect(matchDocumentsWithEmployees({
+      payslipDocuments: [
+        {
+          id: 'abc1',
+          file: {
+            name: 'Расчетный листок за ноябрь Иванов 2023.pdf',
+          },
+        },
+      ],
+      employees: [
+        {
+          lastName: 'Иванов',
+        },
+      ],
+    })).to.deep.equal({
+      abc1: {
+        lastName: 'Иванов',
+      },
+    });
+  });
 });
