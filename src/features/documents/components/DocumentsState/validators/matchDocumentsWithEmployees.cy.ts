1adb3ed6eaef68514f862f5f2a2651aff6894302
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-06-19 09:47:08 +0500

    test: matchDocumentsWithEmployees.cy.ts
        GIVEN two employees
        AND two files with employee's last name in lowercase and uppercase in filename
        WHEN last name matched
        THEN map contains two id-employee relation

--- src/features/documents/components/DocumentsState/validators/matchDocumentsWithEmployees.cy.ts
+++ src/features/documents/components/DocumentsState/validators/matchDocumentsWithEmployees.cy.ts
@@ -45,4 +45,43 @@ describe('matchDocumentsWithEmployees', () => {
       },
     });
   });
+
+  it(`
+    GIVEN two employees
+    AND two files with employee's last name in lowercase and uppercase in filename
+    WHEN last name matched
+    THEN map contains two id-employee relation
+    `, () => {
+    expect(matchDocumentsWithEmployees({
+      payslipDocuments: [
+        {
+          id: 'abc1',
+          file: {
+            name: 'Расчетный листок за ноябрь ПЕТРОВ 2023.pdf',
+          },
+        },
+        {
+          id: 'abc2',
+          file: {
+            name: 'Расчетный листок за ноябрь иванов 2023.pdf',
+          },
+        },
+      ],
+      employees: [
+        {
+          lastName: 'Иванов',
+        },
+        {
+          lastName: 'Петров',
+        },
+      ],
+    })).to.deep.equal({
+      abc1: {
+        lastName: 'Петров',
+      },
+      abc2: {
+        lastName: 'Иванов',
+      },
+    });
+  });
 });
