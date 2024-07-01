149f53ec1f016cade5e841dc0f817726bd5cdc59
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-07-01 10:35:36 +0500

    test: UploadingMailingPayslipsContainer.cy.tsx
      GIVEN upload documents
      WHEN upload new documents
      THEN call request to get employees and trigger validation
      AND render no errors

--- src/features/documents/components/UploadingMailingPayslips/components/UploadingMailingPayslipsContainer.cy.tsx
+++ src/features/documents/components/UploadingMailingPayslips/components/UploadingMailingPayslipsContainer.cy.tsx
@@ -69,6 +69,35 @@ describe('UploadingMailingPayslipsContent', () => {
       .contains('Network Error')
       .should('exist');
   });
+
+  it(`
+  GIVEN upload documents
+  WHEN upload new documents
+  THEN call request to get employees and trigger validation
+  AND render no errors 
+  `, () => {
+    mountComponent();
+
+    cy
+      .intercept('GET', `${API_ROOT}${LINK_TO_DOCUMENTS_SERVICE}getEmployees`, {})
+      .as('get-employees');
+
+    cy
+      .get('input[type=file]')
+      .selectFile([
+        'cypress/fixtures/Расчетный листок Иванов за ноябрь 2023.pdf',
+        'cypress/fixtures/Петров Расчетный NON_EXISTING_TEXT март 2024.pdf',
+      ], {
+        force: true,
+      });
+
+    cy
+      .wait('@get-employees');
+
+    cy
+      .getByData('uploaded-document-card-error')
+      .should('not.exist');
+  });
 });
 
 function mountComponent() {
