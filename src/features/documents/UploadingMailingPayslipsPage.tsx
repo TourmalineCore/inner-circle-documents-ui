b2c477ce20961c5f88de50622e45bd18a7e373dc
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-07-01 10:38:10 +0500

    refactor: add state provider to UploadingMailingPayslipsPage.tsx

--- src/features/documents/UploadingMailingPayslipsPage.tsx
+++ src/features/documents/UploadingMailingPayslipsPage.tsx
@@ -1,7 +1,18 @@
+import { useMemo } from 'react';
 import { UploadingMailingPayslipsContainer } from './components/UploadingMailingPayslips/UploadingMailingPayslipsContainer';
+import { DocumentsState } from './components/DocumentsState/DocumentsState';
+import { DocumentsStateContext } from './components/DocumentsState/DocumentsStateContext';
 
 export function UploadingMailingPayslipsPage() {
+  const documentsState = useMemo(
+    () => new DocumentsState(),
+    [],
+  );
+
   return (
-    <UploadingMailingPayslipsContainer />
+    <DocumentsStateContext.Provider value={documentsState}>
+      <UploadingMailingPayslipsContainer />
+    </DocumentsStateContext.Provider>
+
   );
 }
