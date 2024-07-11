49dd2602b6f313af9565a9905dab1e812c1838b3
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-07-11 10:04:57 +0500

    feat: add Unicode-compatible comparison

--- src/features/documents/sections/state/validators/matchPayslipsWithEmployees.ts
+++ src/features/documents/sections/state/validators/matchPayslipsWithEmployees.ts
@@ -19,8 +19,16 @@ export function matchPayslipsWithEmployees({
     };
   } = {};
 
+  // TODO using Unicode-compatible comparison
+  // This ensures accurate matching even if the names contain Unicode characters with accents or other diacritical marks
+  // Normalization Form D (NFD) - canonical decomposition. Decompose cześć into c, z, e, c + ´, s + ´
   payslips.forEach((payslip) => {
-    const matchingEmployee = employees.find((employee) => payslip.file.name.toLowerCase().includes(employee.lastName.toLowerCase()));
+    const matchingEmployee = employees.find(
+      (employee) => payslip.file.name.toLowerCase()
+        .normalize('NFD').replace(/\p{Diacritic}/gu, '')
+        .includes(employee.lastName.toLowerCase()
+          .normalize('NFD').replace(/\p{Diacritic}/gu, '')),
+    );
 
     if (matchingEmployee) {
       result[payslip.id] = {
