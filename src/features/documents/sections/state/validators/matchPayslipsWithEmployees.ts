a6e747f5d886033a04bf7c7aa5b360552b7d70e4
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-07-11 14:18:23 +0500

    chore: comment Unicode-compatible comparison

--- src/features/documents/sections/state/validators/matchPayslipsWithEmployees.ts
+++ src/features/documents/sections/state/validators/matchPayslipsWithEmployees.ts
@@ -23,12 +23,14 @@ export function matchPayslipsWithEmployees({
   // This ensures accurate matching even if the names contain Unicode characters with accents or other diacritical marks
   // Normalization Form D (NFD) - canonical decomposition. Decompose cześć into c, z, e, c + ´, s + ´
   payslips.forEach((payslip) => {
-    const matchingEmployee = employees.find(
-      (employee) => payslip.file.name.toLowerCase()
-        .normalize('NFD').replace(/\p{Diacritic}/gu, '')
-        .includes(employee.lastName.toLowerCase()
-          .normalize('NFD').replace(/\p{Diacritic}/gu, '')),
-    );
+    const matchingEmployee = employees.find((employee) => payslip.file.name.toLowerCase().includes(employee.lastName.toLowerCase()));
+
+    //const matchingEmployee = employees.find(
+    //    (employee) => payslip.file.name.toLowerCase()
+    //        .normalize('NFD').replace(/\p{Diacritic}/gu, '')
+    //        .includes(employee.lastName.toLowerCase()
+    //            .normalize('NFD').replace(/\p{Diacritic}/gu, '')),
+    //);
 
     if (matchingEmployee) {
       result[payslip.id] = {
