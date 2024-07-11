5df14cdabc0ce5d0caa74de04f8b051f2e19a55f
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-07-11 10:04:32 +0500

    test: refactor UploadingPayslipsContainer.cy.tsx

--- src/features/documents/sections/uploading-payslips-list/UploadingPayslipsContainer.cy.tsx
+++ src/features/documents/sections/uploading-payslips-list/UploadingPayslipsContainer.cy.tsx
@@ -1,20 +1,46 @@
-import { API_ROOT, LINK_TO_DOCUMENTS_SERVICE } from '../../../../common/config/config';
 import { PayslipsState } from '../state/PayslipsState';
 import { PayslipsStateContext } from '../state/PayslipsStateContext';
 import { UploadingPayslipsContainer } from './UploadingPayslipsContainer';
 
 describe('UploadingPayslipsContainer', () => {
+  beforeEach(() => {
+    const EMPLOYEES_RESPONSE = {
+      employees: [
+        {
+          lastName: 'Ivanov',
+        },
+        {
+          lastName: 'Petrov',
+        },
+      ],
+    };
+
+    cy.intercept(
+      'GET',
+      '**/getEmployees',
+      EMPLOYEES_RESPONSE,
+    )
+      .as('get-employees');
+  });
+
+  describe('Error', errorTests);
+
+  describe('E2E', e2eTests);
+});
+
+function e2eTests() {
   it(`
-  GIVEN send payslips
-  WHEN click on the send button 
-  THEN call request to send payslips
+  GIVEN upload payslips
+  WHEN upload new payslips
+  THEN call request to get employees and trigger validation
+  AND render no errors
   `, () => {
     mountComponent();
 
     cy
       .intercept(
         'POST',
-        `${API_ROOT}${LINK_TO_DOCUMENTS_SERVICE}sendMailingPayslips`,
+        '**/sendMailingPayslips',
         {},
       )
       .as('sendPayslips');
@@ -23,9 +49,17 @@ describe('UploadingPayslipsContainer', () => {
       .get('input[type=file]')
       .selectFile([
         'cypress/fixtures/Payslip for Ivanov for November 2023.pdf',
+        'cypress/fixtures/Petrov payslip NON_EXISTING_TEXT for March 2024.pdf',
       ], {
         force: true,
       });
+
+    cy
+      .wait('@get-employees');
+
+    cy
+      .getByData('uploaded-payslip-card-error')
+      .should('not.exist');
 
     cy
       .getByData('uploading-payslips-content-button')
@@ -38,10 +72,12 @@ describe('UploadingPayslipsContainer', () => {
       .getByData('uploading-payslips-content-list')
       .should('not.exist');
   });
+}
 
+function errorTests() {
   it(`
-  GIVEN error message 
-  WHEN call request to send payslips 
+  GIVEN error message
+  WHEN call request to send payslips
   THEN render toasify with error message
   `, () => {
     mountComponent();
@@ -49,7 +85,7 @@ describe('UploadingPayslipsContainer', () => {
     cy
       .intercept(
         'POST',
-        `${API_ROOT}${LINK_TO_DOCUMENTS_SERVICE}sendMailingPayslips`,
+        '**/sendMailingPayslips',
         {
           forceNetworkError: true,
         },
@@ -79,40 +115,7 @@ describe('UploadingPayslipsContainer', () => {
       .contains('Network Error')
       .should('exist');
   });
-
-  it(`
-  GIVEN upload payslips
-  WHEN upload new payslips
-  THEN call request to get employees and trigger validation
-  AND render no errors 
-  `, () => {
-    mountComponent();
-
-    cy
-      .intercept(
-        'GET',
-        `${API_ROOT}${LINK_TO_DOCUMENTS_SERVICE}getEmployees`,
-        {},
-      )
-      .as('get-employees');
-
-    cy
-      .get('input[type=file]')
-      .selectFile([
-        'cypress/fixtures/Payslip for Ivanov for November 2023.pdf',
-        'cypress/fixtures/Petrov payslip NON_EXISTING_TEXT for March 2024.pdf',
-      ], {
-        force: true,
-      });
-
-    cy
-      .wait('@get-employees');
-
-    cy
-      .getByData('uploaded-payslip-card-error')
-      .should('not.exist');
-  });
-});
+}
 
 function mountComponent() {
   // eslint-disable-next-line react/jsx-no-constructed-context-values
