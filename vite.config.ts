c73dd9a615ccb0f98a4edac7abbbf5fbcd308d43
Maxim Rychkov <rychkov03@yandex.ru>
2025-11-13 11:54:02 +0500

    docs: #41: remove commented rudimentary vars

--- vite.config.ts
+++ vite.config.ts
@@ -15,15 +15,6 @@ const DOCUMENTS_PORT = process.env.NODE_ENV === `production` ? LOCAL_ENV_PORT : 
 
 const BASE_PATH = '/documents'
 
-// if (VITE_BASE_URL === undefined) set default local docker url
-// const LAYOUT_URL = process.env.VITE_BASE_URL ?? `http://localhost:4455`
-
-// for run in local docker use `/`, and for others use `/books` path
-// const BOOKS_PATH = LAYOUT_URL === `http://localhost:4455` ? `/` : `/books`
-
-// for run in local docker use `http://localhost:4455/assets..`, and for others use `../layout/assets..` path
-// const LAYOUT_PATH = LAYOUT_URL === `http://localhost:4455` ? `` : `/layout`
-
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   // Set the port on which the development server runs
